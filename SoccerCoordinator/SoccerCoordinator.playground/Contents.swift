c02b09d16953a350d0c53987a7a500ac8f8951bc
Jesse Anderson <sockeyes51@gmail.com>
2016-07-05 12:05:34 -0400

    Added in all the players and created the teams arrays.

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -2,7 +2,53 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var teamDragons = []
+var teamSharks = []
+var teamRaptors = []
 
 
-// First Commit
+// Create each player and add in the information to the dictionary
+// A player class could be created to make sure all this information is standardized and added in approperiatly
+
+let joeSmith = ["name": "Joe Smith", "height": 42, "experiance": true, "guardian": "Jim and Jan Smith"]
+let jillTanner = ["name": "Jill Tanner", "height": 36, "experiance": true, "guardian": "Clara Tanner"]
+let billBon = ["name": "Bill Bon", "height": 43, "experiance": true, "guardian": "Sarah and Jenny Bon"]
+let evaGordon = ["name": "Eva Gordon", "height": 45, "experiance": false, "guardian": "Wendy and Mike Gordon"]
+let mattGill = ["name": "Matt Gill", "height": 40, "experiance": false, "guardian": "Charles and Sylvia Gill"]
+let kimmyStein = ["name": "Kimmy Stein", "height": 41, "experiance": false, "guardian": "Bill and Hillary Stein"]
+let sammyAdams = ["name": "Sammy Adams", "height": 45, "experiance": false, "guardian": "Jeff Adams"]
+let karlSaygan = ["name": "Karl Saygan", "height": 42, "experiance": true, "guardian": "Heather Bledsoe"]
+let suzaneGreenberg = ["name": "Suzane Greenberg", "height": 44, "experiance": true, "guardian": "Henrietta Dumas"]
+let salDali = ["name": "Sal Dali", "height": 41, "experiance": false, "guardian": "Gala Dali"]
+let joeKavalier = ["name": "Joe Kavalier", "height": 39, "experiance": false, "guardian": "Sam and Elaine Kavalier"]
+let benFinkelstein = ["name": "Ben Finkelstein", "height": 44, "experiance": false, "guardian": "Arron and Jill Finkelstein"]
+let diegoSoto = ["name": "Diego Soto", "height": 41, "experiance": true, "guardian": "Robin and Sarika Soto"]
+let chloeAlaska = ["name": "Chloe Alaska", "height": 47, "experiance": false, "guardian": "David and Jamie Alaska"]
+let arnoldWillis = ["name": "Arnold Willis", "height": 43, "experiance": false, "guardian": "Claire Willis"]
+let phillipHelm = ["name": "Phillip Helm", "height": 44, "experiance": true, "guardian": "Thomas Helm and Eva Jones"]
+let lesClay = ["name": "Les Clay", "height": 42, "experiance": true, "guardian": "Wynonna Brown"]
+let herschelKrustofski = ["name": "Herschel Krustofski", "height": 45, "experiance": true, "guardian": "Hyman and Rachel Krustofski"]
+
+
+// Add all the players to the players array
+
+let players = [joeSmith,
+               jillTanner,
+               billBon,
+               evaGordon,
+               mattGill,
+               kimmyStein,
+               sammyAdams,
+               karlSaygan,
+               suzaneGreenberg,
+               salDali,
+               joeKavalier,
+               benFinkelstein,
+               diegoSoto,
+               chloeAlaska,
+               arnoldWillis,
+               phillipHelm,
+               lesClay,
+               herschelKrustofski]
+
+
